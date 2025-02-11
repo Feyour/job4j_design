3fbc773f96466b4cac812f04f16ff913bea6374f
feyour <feyour@gmail.com>
2025-02-11 19:09:16 +0300

    0. Select [#6861]

---
+++ src/main/java/ru/job4j/db/insert_fauna.sql
@@ -0,0 +1,12 @@
+insert into fauna (name, avg_age, discovery_date)
+values('Домашняя мышь', 730, '1758-09-01');
+insert into fauna (name, avg_age, discovery_date)
+values('Кошка', 5475, '1758-09-01');
+insert into fauna (name, avg_age, discovery_date)
+values('Слон', 21900, '1797-09-01');
+insert into fauna (name, avg_age, discovery_date)
+values('Лиса', 3650, '1758-09-01');
+insert into fauna (name, avg_age, discovery_date)
+values('Пингвин ', 7665, '1844-09-01');
+insert into fauna (name, avg_age, discovery_date)
+values('Кит', 73000, '1758-09-01');
---
+++ src/main/java/ru/job4j/db/select_fauna.sql
@@ -0,0 +1,7 @@
+select * from fauna where name LIKE'%fish%';
+
+select * from fauna where avg_age >= 10000 AND	avg_age < 21000;
+
+select * from fauna where discovery_date is null;
+
+select * from fauna where discovery_date < '1950-01-01';
